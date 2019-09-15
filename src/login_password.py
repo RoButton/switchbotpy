33d20c1e970a32f4daf9465c835facf3d22c87c5
nicolas-kuechler <nico.kuechler@gmail.com>
2019-09-15 16:52:26 +0200

    fix in utility script

--- src/login_password.py
+++ src/login_password.py
@@ -12,7 +12,9 @@ pw = getpass.getpass(prompt='Login Password (required for receiving bearer token
 pw_hash = bcrypt.generate_password_hash(pw)
 keyring.set_password(SERVICE_NAME, 'login_password_hash', pw_hash.decode('utf-8'))
 
-if not input("Do you want to invalidate all login tokens? (y/n): ").lower().strip()[:1] == "y": 
+if input("Do you want to invalidate all login tokens? (y/n): ").lower().strip()[:1] == "y": 
+    print("Deleting jwt secret...")
     keyring.delete_password(SERVICE_NAME, 'jwt_secret_key')
+    
 
 print("Changes Applied! Don't forget to restart webserver (e.g. Gunicorn) for changes to become active.")
