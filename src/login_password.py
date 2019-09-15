c6bc460b9627c415922a76d0a77bf53fbb313c37
nicolas-kuechler <nico.kuechler@gmail.com>
2019-09-15 16:44:17 +0200

    add token secret reset to login password script

--- src/login_password.py
+++ src/login_password.py
@@ -12,4 +12,7 @@ pw = getpass.getpass(prompt='Login Password (required for receiving bearer token
 pw_hash = bcrypt.generate_password_hash(pw)
 keyring.set_password(SERVICE_NAME, 'login_password_hash', pw_hash.decode('utf-8'))
 
-print("Login Password Changed! Don't forget to restart webserver (e.g. Gunicorn) for changes to become active.")
+if not input("Do you want to invalidate all login tokens? (y/n): ").lower().strip()[:1] == "y": 
+    keyring.delete_password(SERVICE_NAME, 'jwt_secret_key')
+
+print("Changes Applied! Don't forget to restart webserver (e.g. Gunicorn) for changes to become active.")
