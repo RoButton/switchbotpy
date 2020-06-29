16abfed3b8325cd65e3933ebcc583188b5addd8b
nicolas-kuechler <nico.kuechler@protonmail.com>
2020-06-29 17:31:32 +0200

    add action status to init

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
     name='switchbotpy',
     packages=['switchbotpy'],
-    version='0.1.6',
+    version='0.1.7',
     license='MIT',
     description='An API for Switchbots that allows to control actions, settings and timers (also password protected)',
     long_description=long_description,
@@ -14,7 +14,7 @@ setup(
     author='Nicolas Küchler',
     author_email='nico.kuechler@protonmail.com',
     url='https://github.com/RoButton/switchbotpy',
-    download_url='https://github.com/RoButton/switchbotpy/archive/v_016.tar.gz',
+    download_url='https://github.com/RoButton/switchbotpy/archive/v_017.tar.gz',
     keywords=['Switchbot', 'Ble', 'Button', 'Actions', 'Settings', 'Timers'],
     install_requires=['pygatt', 'pexpect'],
     classifiers=[
